1b082458854eb3462ea617c5d078ffc7ce639e98
DrJume <DrJume@users.noreply.github.com>
2017-03-08 16:44:46 +0100

    Schul-Edit 2

--- CRON.js
+++ CRON.js
@@ -1,7 +1,7 @@
 const shell = require('child_process').exec;
 
 console.log("Der Update-Cronjob wird eingerichtet...");
-shell('echo "0 4 * * * pi node ' + __dirname + '/UPDATE.js" > /etc/cron.d/manosVplan', function (error, stdout, stderr) {
+shell('echo "0 4 * * * pi node ' + __dirname + '/UPDATE.js\n@reboot pi cd ' + __dirname + ' && npm start " > /etc/cron.d/manosVplan', function (error, stdout, stderr) {
   if (error) {
     console.error(error);
     return;
